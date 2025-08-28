b4ae6bd0018c15446c956591d8a96e03d289bf5b
iCodeLakshay <lakshaysaxena2217@gmail.com>
2025-08-28 23:00:22 +0530

    Add logout route to authentication API

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getLoggedInUser, login, signup } from "../controllers/auth.controller.js";
+import { getLoggedInUser, login, logout, signup } from "../controllers/auth.controller.js";
 import { verifyUser } from "../Middlewares/verifyUser.js";
 
 const router = Router();
@@ -7,5 +7,6 @@ const router = Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.get("/me", verifyUser, getLoggedInUser);
+router.get("/logout", verifyUser, logout);
 
 export default router;
